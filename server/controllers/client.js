98afb7980595124a941480baa2aedbb343186657
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-01-12 00:08:39 +0545

    customers page

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,4 +1,5 @@
 import Product from "../models/Product.js";
+import User from '../models/User.js';
 import ProductStat from "../models/ProductStat.js";
 
 export const getProducts = async(req, res) => {
@@ -24,3 +25,12 @@ export const getProducts = async(req, res) => {
         res.status(404).json({ message: error.message });
     }
 } 
+
+export const getCustomers = async(req, res) => {
+    try{
+        const customers = await User.find({ role: "user" }).select("-password");
+        res.status(200).json(customers);
+    } catch(err){
+        res.status(404).json({ message: error.message });
+    }
+}
