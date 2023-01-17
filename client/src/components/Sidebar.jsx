ef330be35de752d18039e0d62c6db737420b16e8
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-01-18 00:32:07 +0545

    change

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -132,7 +132,7 @@ const Sidebar = ({
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display="flex" alignItems="center" gap="0.5rem">
                   <Typography variant="h4" fontWeight="bold">
-                    ACE PHARMA
+                    ACE PHARMACY
                   </Typography>
                 </Box>
                 {!isNonMobile && (
